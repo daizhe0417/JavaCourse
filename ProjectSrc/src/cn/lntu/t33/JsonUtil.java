974cd9c587ab2b1a3abaebf4f2b21902666fda21
Administrator <Administrator@PC201406251758>
2015-05-10 20:22:12 +0800

    hjkhj

---
+++ ProjectSrc/src/cn/lntu/t33/JsonUtil.java
@@ -0,0 +1,20 @@
+package com.bookMange.json;
+
+import com.google.gson.Gson;
+
+public class JsonUtil {
+    private static Gson gson = buildGson();
+
+    private static Gson buildGson() {
+        Gson gson = new Gson();
+        return gson;
+    }
+
+    public static String toJson(Object src) {
+        return gson.toJson(src);
+    }
+
+    public static <T>T fromJson(String json, Class<T> classOfT) {
+        return gson.fromJson(json, classOfT);
+    }
+}
